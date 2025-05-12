cb70409201d7f7320f7f747cc0e9ec4f389d11cf
Malika Personal <malikasri1994@gmail.com>
2025-05-12 13:02:21 +0530

    initial commit

---
+++ django_client/greet_app/views.py
@@ -0,0 +1,17 @@
+# django_client/greet_app/views.py
+
+from django.http import JsonResponse
+import grpc
+from .proto import greet_pb2
+from .proto import greet_pb2_grpc
+
+
+def hello_view(request):
+    name = request.GET.get("name", "World")
+
+    # Connect to gRPC server
+    with grpc.insecure_channel("localhost:50051") as channel:
+        stub = greet_pb2_grpc.GreeterStub(channel)
+        response = stub.SayHello(greet_pb2.HelloRequest(name=name))
+
+    return JsonResponse({"message": response.message})
---
+++ go-greeter/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net"
+
+	pb "go-greeter/proto" // using relative import since everything is in one module
+
+	"google.golang.org/grpc"
+)
+
+type server struct {
+	pb.UnimplementedGreeterServer
+}
+
+func (s *server) SayHello(ctx context.Context, req *pb.HelloRequest) (*pb.HelloReply, error) {
+	log.Printf("Received: %s", req.Name)
+	return &pb.HelloReply{Message: "Hello " + req.Name}, nil
+}
+
+func main() {
+	lis, err := net.Listen("tcp", ":50051")
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	grpcServer := grpc.NewServer()
+	pb.RegisterGreeterServer(grpcServer, &server{})
+	log.Println("gRPC server is running on port 50051...")
+	if err := grpcServer.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
